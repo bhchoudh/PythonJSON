ac1ef1338a08590d0447fde2c28f26fd18e0ce6e
bhchoudh <39667569+bhchoudh@users.noreply.github.com>
2018-07-12 13:37:16 +0530

    dict key value handling added

--- JSONRead.py
+++ JSONRead.py
@@ -4,12 +4,12 @@ import json
 filepath=input(" JSON file path with forward / :-")
 fileref=open(filepath,'w')
 #Prepare an array & non array dictionary & convert into a JSOn object to write in a JSON file
-data={'TypeA':{'CarModel': 'Renault', 'price': 600, 'Country': 'France'},'TypeB':{'CarModel': 'Ford', 'price': 200, 'Country': 'USA'}}
-#data=[{'CarModel': 'Renault', 'price': 600, 'Country': 'France'},{'CarModel': 'Ford', 'price': 200, 'Country': 'USA'}]
+#data={'TypeA':{'CarModel': 'Renault', 'price': 600, 'Country': 'France'},'TypeB':{'CarModel': 'Ford', 'price': 200, 'Country': 'USA'}}
+data=[{'CarModel': 'Renault', 'price': 600, 'Country': 'France'},{'CarModel': 'Ford', 'price': 200, 'Country': 'USA'}]
 print(data)
 fileref.write(json.dumps(data,indent=4))
 
-quit()
+#quit()
 
 #Read an Non-array from JSON file , access data by item key
 filepath=input("nonarray JSON file path with forward / :-")
@@ -19,8 +19,10 @@ print("Priniting Non Array JSON *************")
 print (json.dumps(data,indent=4))
 #If item keys are known then use *data["BMW"]* to access the value of that key
 #If item key are not known , fetch each key in a for loop and then use that key to get value
-for item in data:
-    print(data[item])
+for eachrow in data:
+    print(data[eachrow])
+    for key,val  in data[eachrow].items():
+        print(key,val)
 
 
 #Read an array from JSON file , use for loop to read array items
@@ -30,8 +32,9 @@ data=json.load(fileref)     #OR data=json.loads(fileref.read())
 print("Priniting  Array JSON *************")
 print (json.dumps(data,indent=4))
 #Array elements are accessed automatically using for loop
-for item in data:
-    print(item)
+for eachrow in data:
+    print(eachrow)
+    for key,val in eachrow.items():
+        print (key,val)
 
 #quit()
-
